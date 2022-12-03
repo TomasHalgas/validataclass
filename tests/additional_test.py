9a73fcb80b89f5cd8ea6eb1023bd0a70cb69ed4d
th121pu <r77KJTfs>
2022-12-03 12:10:17 +0100

    TESTS: integration tests

--- tests/additional_test.py
+++ tests/additional_test.py
@@ -7,8 +7,9 @@ Use of this source code is governed by an MIT-style license that can be found in
 import pytest
 from pytest import raises
 from validataclass.exceptions import InvalidDecimalError
-from validataclass.validators import DecimalValidator, IntegerValidator, Validator
-from validataclass.dataclasses import validataclass
+from validataclass.validators import DecimalValidator, IntegerValidator, Validator, BooleanValidator, StringValidator, \
+    AnythingValidator, ListValidator, UrlValidator, Noneable, RejectValidator, DataclassValidator
+from validataclass.dataclasses import validataclass, ValidataclassMixin
 from unittest.mock import MagicMock
 from validataclass.helpers import BaseDateTimeRange
 import unittest.mock
@@ -16,9 +17,14 @@ import sys
 
 
 class MockedPattern:
-
     def fullmatch(self):
         return True
+
+
+@validataclass
+class AdditionalTestDataclass(ValidataclassMixin):
+    url: str = UrlValidator()
+    boolean: bool = (Noneable(RejectValidator(), default=True))
 
 
 class AdditionalTest:
@@ -63,3 +69,53 @@ class AdditionalTest:
     """
     Additional integration tests.
     """
+
+    @staticmethod
+    def test_integration_string_boolean():
+        # check workflow of StringValidator(min_length=3, max_length=5) + BooleanValidator(allow_strings=True)
+        # if it accepts string value "true"
+        validator_string = StringValidator(min_length=3, max_length=5)
+        string_result = validator_string.validate("true")
+        validator_boolean = BooleanValidator(allow_strings=True)
+        boolean_result = validator_boolean.validate(string_result)
+        assert boolean_result is True
+
+    @staticmethod
+    def test_integration_anything_list():
+        # check workflow: if combination of AnythingValidator + ListValidator accepts anything
+        validator_anything = AnythingValidator(allowed_types=[int, float, str, list, tuple, range, bool, dict, complex],
+                                               allow_none=True)
+        input_list = [
+            42,
+            4.2,
+            '1.234',
+            'banana',
+            [8, 4],
+            [8, 4.2],
+            [8, 'banana'],
+            ("apple", "banana", "cherry"),
+            range(1, 80, 10),
+            True,
+            False,
+            {
+                "a": 4,
+                "b": 5
+            },
+            complex(2, -3),
+            None,
+        ]
+
+        validator_list = ListValidator(validator_anything)
+        list_result = validator_list.validate(input_list)
+
+        assert list_result == input_list
+
+    @staticmethod
+    def test_dataclass_url_noneable_reject_validators():
+        # check workflow of DataclassValidator with custom scheme (UrlValidator + Noneable wrapper with RejectValidator)
+        validator = DataclassValidator(AdditionalTestDataclass)
+        obj: AdditionalTestDataclass = validator.validate({'url': "https://moodle.fei.tuke.sk", 'boolean': None})
+        assert obj.to_dict() == {
+            'url': "https://moodle.fei.tuke.sk",
+            'boolean': True
+        }
